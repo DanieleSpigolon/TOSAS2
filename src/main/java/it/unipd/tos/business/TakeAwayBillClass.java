8639c558fbe7634787bdb250099e21f535715bf2
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2020-11-24 19:27:10 +0100

    Aggiunti test per #2. close #2

--- src/main/java/it/unipd/tos/business/TakeAwayBillClass.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillClass.java
@@ -6,6 +6,7 @@ package it.unipd.tos.business;
 import java.util.List;
 
 import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.ItemType;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
 
@@ -13,13 +14,27 @@ public class TakeAwayBillClass implements TakeAwayBill {
 
     public double getOrderPrice(List<MenuItem> itemsOrdered,User user) 
             throws TakeAwayBillException {
-        double totalPrice = 0.0;
 
+        double totalPrice = 0.0;
+        double lowPrice= Double.MAX_VALUE;
+        double totalDiscount = 0.0;
+        int gelatiNumber = 0;
+        
         for(MenuItem itemOrdered : itemsOrdered) {
             totalPrice += itemOrdered.getPrice();
-        }
 
-        return totalPrice;
+            if(itemOrdered.getItemType() == ItemType.Gelati) {
+                if(itemOrdered.getPrice() < lowPrice) {
+                    lowPrice = itemOrdered.getPrice();
+                }  
+               gelatiNumber++;
+            }  
+        }
+        
+        if(gelatiNumber > 5) {
+            totalDiscount += lowPrice / 2.0;
+        }
+        return totalPrice - totalDiscount;
     }
 
 }
