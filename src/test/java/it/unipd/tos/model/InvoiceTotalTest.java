7e7fb3a7fbc5f16384d99992e6f3f48b0004fc51
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2020-12-01 22:20:24 +0100

    Modificati test InvoiceTotalTest

--- src/test/java/it/unipd/tos/model/InvoiceTotalTest.java
+++ src/test/java/it/unipd/tos/model/InvoiceTotalTest.java
@@ -1,13 +1,15 @@
 ////////////////////////////////////////////////////////////////////
 // [DANIELE] [SPIGOLON] [1193290]
 ////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos.model;
+
 import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
-
 import it.unipd.tos.model.InvoiceTotal;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
@@ -15,18 +17,36 @@ import it.unipd.tos.model.ItemType;
 
 public class InvoiceTotalTest {
 
-    @Test
-    public void InvoiceTotalFirstTest(){
-        List<MenuItem> listaOrder = new ArrayList<MenuItem>();
-        listaOrder.add(new MenuItem(ItemType.Bevande, "LemonSoda", 3.00));
+    private User user;
+    private List<MenuItem> lista;
+    private InvoiceTotal invoiceTotal;
 
-        User user = new User("110","Daniele", "Spigolon", 2);
-
-        InvoiceTotal InvoiceTotal = new InvoiceTotal(listaOrder, user, 2008, 7.00);
-
-        assertEquals(listaOrder, InvoiceTotal.getLista());
-        assertEquals(user, InvoiceTotal.getUser());
-        assertEquals(2008,InvoiceTotal.gettempoInSecondi());
-        assertEquals(7.00,InvoiceTotal.getPrice(),0);
+    @Before
+    public void setup(){
+        user = new User("110","Daniele","Spigolon",18);
+        lista = new ArrayList<MenuItem>();
+        invoiceTotal = new InvoiceTotal(lista, user, 1336, 3.00);
+        lista.add(new MenuItem(ItemType.Bevande, "LemonSoda", 3.00));
     }
+
+    @Test
+    public void getListaTest() {
+        assertEquals(lista, invoiceTotal.getLista());
+    }
+
+    @Test
+    public void getUserTest() {
+        assertEquals(user, invoiceTotal.getUser());
+    }
+
+    @Test
+    public void getTimeTest() {
+        assertEquals(1336,invoiceTotal.gettempoInSecondi());
+    }
+
+    @Test
+    public void getPriceTest() {
+        assertEquals(3.00,invoiceTotal.getPrice(),0);
+    }
+
 }
